d3b1de64ef84f952d931c150c213fcbae1d2670e
Coenen <houkenan0620@126.com>
2017-11-29 13:26:34 +0800

    171129、项目初始化
    初始化创建项目。

---
+++ Swift4/Swift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4
+//
+//  Created by Conan on 2017/11/29.
+//  Copyright © 2017年 Conan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

e410140ace6d40fa1b1e98c1313677fac40af8a5
Coenen <houkenan0620@126.com>
2017-11-29 13:40:07 +0800

    171128、初始化

---
+++ Swift4/Swift4/FirstVC.swift
@@ -0,0 +1,39 @@
+//
+//  FirstVC.swift
+//  Swift4
+//
+//  Created by Conan on 2017/11/29.
+//  Copyright © 2017年 Conan. All rights reserved.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    @IBOutlet weak var basisUIbuttonClick: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

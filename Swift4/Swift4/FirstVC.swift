f82ffaccf37f1d15bbd75f1868b042162ac46c0a
Coenen <houkenan0620@126.com>
2017-11-29 15:08:00 +0800

    171129、基本控件的使用;UIButton、UITextField、UISwitch、UITextView、UISlider、UIView、UIImageView、UILabel

--- Swift4/Swift4/FirstVC.swift
+++ Swift4/Swift4/FirstVC.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class FirstVC: UIViewController {
-
-    @IBOutlet weak var basisUIbuttonClick: UIButton!
     
+    @IBOutlet weak var basisBtn: UIButton!
+    @IBOutlet weak var basisTextField: UITextField!
+    @IBOutlet weak var basisSwitch: UISwitch!
+    @IBOutlet weak var basisTextView: UITextView!
+    @IBOutlet weak var basisSliderView: UISlider!
+    @IBOutlet weak var basisView: UIView!
+    @IBOutlet weak var basisImageView: UIImageView!
+    @IBOutlet weak var basisLab: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+       
     }
 
+
+    @IBAction func basisBtnClick() {
+        print("basisBtnClick")
+    }
     
+    @IBAction func basisSwitchClick(_ sender: UISwitch) {
+        if (sender.isOn) {
+            print("UISwitch on")
+        }else{
+            print("UISwitch off")
+        }
+        
+    }
+    
+    @IBAction func basisSilderClick(_ sender: UISlider) {
+        print("basisSilderClick~%f",sender.value)
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.basisTextView.resignFirstResponder()
+        self.basisTextField.resignFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
